1d1d7f9ae60fd7124eff6bfc08da99b6ed740f01
jrodriguez86 <57810392+jrodriguez86@users.noreply.github.com>
2020-02-01 15:05:16 -0500

    New repo with correct domain

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("it works!")
+
+$(() => {
+     $("form").on("submit", (event) => {
+         event.preventDefault();
+
+         const userInput = $('input[type="text"]').val();
+
+         $.ajax({
+            url: "https://gateway.marvel.com:443/v1/public/characters?apikey=cd85b6020ae0420df7a8b994940161dd"
+            
+            
+           }).then(
+               (data)=>{
+                   console.log(data);
+               }
+           );
+     })
+
+
+
+
+
+})
