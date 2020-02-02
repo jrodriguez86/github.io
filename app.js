0c775b92a1714e0fd51211c45373a6ccfc5679cc
jrodriguez86 <57810392+jrodriguez86@users.noreply.github.com>
2020-02-02 08:11:09 -0500

    Testing character pull

--- app.js
+++ app.js
@@ -7,15 +7,28 @@ $(() => {
          const userInput = $('input[type="text"]').val();
 
          $.ajax({
-            url: "https://gateway.marvel.com:443/v1/public/characters?apikey=cd85b6020ae0420df7a8b994940161dd" + userInput
-            
+            url: "https://gateway.marvel.com:443/v1/public/characters?apikey=cd85b6020ae0420df7a8b994940161dd" + userInput,
+            method: "GET"
             
             
            }).then(
                (data)=>{
+                console.log(data)
+                   const characterData = data.results[0];
+                   $(".card-title").append(characterData.name);
+
+               }
+
                 
+
                 
-                    console.log(data)
+
+
+                // $('.card-body').append(characterData);
+
+
+                
+                    // console.log(data)
                     // description (string, optional): A short bio or description of the character.,
                     // modified (Date, optional): The date the resource was most recently modified.,
                     // resourceURI (string, optional): The canonical URL identifier for this resource.,
@@ -25,7 +38,7 @@ $(() => {
                     // stories (StoryList, optional): A resource list of stories in which this character appears.,
                     // events (EventList, optional): A resource list of events in which this character appears.,
                     // series (SeriesList, optional): A resource list of series in which this character appears.
-                    }
+           
                    
                 
            );
